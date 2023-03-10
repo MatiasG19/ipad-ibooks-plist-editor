6d0eaf03ef52f789142ab7a26c9fd2eed62d008c
MatiasG19 <m.galusek@gmail.com>
2023-03-10 11:54:50 +0100

    Make usable

--- Program.cs
+++ Program.cs
@@ -1,20 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
 using ExtensionMethods;
 
-//Console.WriteLine("Enter path to .plist file:");
-//string pathToPlist = Console.Read();
+Console.WriteLine("Enter path to .plist file:");
+string plistPath = Console.ReadLine();
 
-string plistContent = File.ReadAllText("Purchases.plist");
+Console.WriteLine("Reading file content...");
+string plistContent = File.ReadAllText(plistPath);
 
-plistContent = plistContent.ReplaceLastOccurrence("</plist>", "")
-                           .ReplaceLastOccurrence("</dict>", "")
-                           .ReplaceLastOccurrence("</array>", "");
+plistContent = plistContent.ReplaceLastOccurrence("</plist>\n", "")
+                           .ReplaceLastOccurrence("</dict>\n", "")
+                           .ReplaceLastOccurrence("</array>\n", "");
 
 // TODO Filter files that already exist
 string[] filePaths = Directory.GetFiles(".", "*.pdf", SearchOption.TopDirectoryOnly);
 
 const string bookEntryTemplate = 
-		@"<dict>
+		"\t" +
+		 @"<dict>
 			<key>Inserted-By-iBooks</key>
 			<true/>
 			<key>Name</key>
@@ -24,29 +26,41 @@ const string bookEntryTemplate =
 			<key>Path</key>
 			<string>%bookNameWithExtension%</string>
 			<key>Persistent ID</key>
-			<string>4B6C69B27ED443C5</string>
+			<string>%presistantId%</string>
 			<key>Persistent ID Generated On Device</key>
-			<true/>
+			<false/>
 			<key>importDate</key>
 			<date>2022-08-28T09:40:30Z</date>
 		</dict>";
 
+Console.WriteLine("Creating entries...");
 foreach (var filePath in filePaths)
 {
     string fileName = filePath.Substring(filePath.LastIndexOf("/") + 1);
+	Console.WriteLine($"Creating entry for: {fileName}");
 
+	// TODO import date
     string bookEntry = bookEntryTemplate;
     string bookNameWithExtenstion = fileName;
     string bookName = fileName.Substring(0, fileName.LastIndexOf(".pdf"));
-    string packageHash = Guid.NewGuid().ToString().Replace("-", "");
+    string packageHash = Guid.NewGuid().ToString().Replace("-", "").ToUpper();
+    string presistantId = packageHash.Substring(0, 16);
 
     bookEntry = bookEntry.Replace("%bookNameWithExtension%", bookNameWithExtenstion);
     bookEntry = bookEntry.Replace("%bookName%", bookName);
-    bookEntry = bookEntry.Replace("%packageHash%", packageHash); // E9245C7C48E4134D741E1939B04FB022
+    bookEntry = bookEntry.Replace("%packageHash%", packageHash); // e.g. E9245C7C48E4134D741E1939B04FB022
+    bookEntry = bookEntry.Replace("%presistantId%", presistantId); // e.g. 4B6C69B27ED443C4
 
     plistContent = string.Concat(new string[] { plistContent, bookEntry + "\n" });    
 }
 
-plistContent = string.Concat(new string[] { plistContent, "</array>\n", "</dict>\n", "</plist>\n"});
+plistContent = string.Concat(new string[] { plistContent, "\t</array>\n", "</dict>\n", "</plist>\n"});
 
-Console.WriteLine(plistContent);
+Console.WriteLine("Writing to plist file...");
+using (FileStream fs = File.Create(plistPath)) 
+{
+	byte[] info = new UTF8Encoding(true).GetBytes(plistContent);
+	fs.Write(info, 0, info.Length);
+}
+
+Console.WriteLine("Finished.");
