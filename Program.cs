f738b7d415cbd564d3e42fe6c3489885736c2dab
MatiasG19 <m.galusek@gmail.com>
2023-03-10 21:17:58 +0100

    Filter out existing entries

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
 ﻿using System.Text;
 using ExtensionMethods;
 
-Console.WriteLine("Enter path to .plist file:");
-string plistPath = Console.ReadLine();
+Console.WriteLine("Enter path to .plist file (e.g. /myFolder/Purchases.plist):");
+string? plistPath = Console.ReadLine();
+if(String.IsNullOrEmpty(plistPath)) 
+{	
+	Console.WriteLine("Invalid path.");	
+	return;
+}
 
 Console.WriteLine("Reading file content...");
 string plistContent = File.ReadAllText(plistPath);
+if(String.IsNullOrEmpty(plistContent)) 
+{
+	Console.WriteLine("Invalid file content.");	
+	return;
+}
 
 plistContent = plistContent.ReplaceLastOccurrence("</plist>\n", "")
                            .ReplaceLastOccurrence("</dict>\n", "")
@@ -34,10 +44,20 @@ const string bookEntryTemplate =
 		</dict>";
 
 Console.WriteLine("Creating entries...");
+int i = 0;
 foreach (var filePath in filePaths)
 {
     string fileName = filePath.Substring(filePath.LastIndexOf("/") + 1);
-	Console.WriteLine($"Creating entry for: {fileName}");
+	if(plistContent.Contains(fileName)) 
+	{
+		Console.WriteLine($"Skipping '{fileName}'. Entry already exists.");
+		continue;
+	}
+	else 
+	{
+		Console.WriteLine($"Creating entry for: {fileName}");
+		i++;
+	}
 
 	// TODO import date
     string bookEntry = bookEntryTemplate;
@@ -54,13 +74,16 @@ foreach (var filePath in filePaths)
     plistContent = string.Concat(new string[] { plistContent, bookEntry + "\n" });    
 }
 
-plistContent = string.Concat(new string[] { plistContent, "\t</array>\n", "</dict>\n", "</plist>\n"});
-
-Console.WriteLine("Writing to plist file...");
-using (FileStream fs = File.Create(plistPath)) 
+if(i > 0) 
 {
-	byte[] info = new UTF8Encoding(true).GetBytes(plistContent);
-	fs.Write(info, 0, info.Length);
-}
+	plistContent = string.Concat(new string[] { plistContent, "\t</array>\n", "</dict>\n", "</plist>\n"});
 
-Console.WriteLine("Finished.");
+	Console.WriteLine("Writing to plist file...");
+	using (FileStream fs = File.Create(plistPath)) 
+	{
+		byte[] info = new UTF8Encoding(true).GetBytes(plistContent);
+		fs.Write(info, 0, info.Length);
+	}
+} 
+
+Console.WriteLine($"Finished. {i} entries written.");
